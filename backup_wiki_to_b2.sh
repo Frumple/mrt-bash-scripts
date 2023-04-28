33a44296e62165b1573d2cb66439009c5109e11d
Frumple <frumple1@gmail.com>
2023-04-28 05:13:31 +0000

    Finish backup_wiki_to_b2.sh.

--- backup_wiki_to_b2.sh
+++ backup_wiki_to_b2.sh
@@ -6,6 +6,27 @@ source ${SCRIPT_DIR}/config/wiki_config
 source ${SCRIPT_DIR}/lib/wiki_utils.sh
 source ${SCRIPT_DIR}/dump_wiki_database_to_file.sh
 
+backup_wiki_to_b2()
+{
+  enter_wiki_readonly_mode
+  dump_wiki_database_to_file
+
+  export B2_ACCOUNT_ID
+  export B2_ACCOUNT_KEY
+  export RESTIC_REPOSITORY
+  export RESTIC_PASSWORD
+
+  cleanup_old_b2_backups
+  run_b2_backup
+
+  unset B2_ACCOUNT_ID
+  unset B2_ACCOUNT_KEY
+  unset RESTIC_REPOSITORY
+  unset RESTIC_PASSWORD
+
+  exit_wiki_readonly_mode
+}
+
 enter_wiki_readonly_mode()
 {
   printf "${MEDIAWIKI_READONLY_MESSAGE}" >> "${MEDIAWIKI_READONLY_LOCK_FILE}"
@@ -18,13 +39,14 @@ exit_wiki_readonly_mode()
   set_wiki_sitenotice ""
 }
 
-backup_wiki_to_b2()
+cleanup_old_b2_backups()
 {
-  enter_wiki_readonly_mode
+  restic forget ${B2_FORGET_OPTIONS}
+}
 
-  dump_wiki_database_to_file
-
-  exit_wiki_readonly_mode
+run_b2_backup()
+{
+  restic backup ${B2_BACKUP_OPTIONS} ${B2_SOURCE}
 }
 
 backup_wiki_to_b2
