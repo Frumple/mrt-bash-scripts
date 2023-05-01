695671333c427b0a08a3de7550f354fb1b1e2b27
Frumple <frumple1@gmail.com>
2023-05-01 02:20:51 +0000

    Update vote reminder message.

--- print_vote_reminder.sh
+++ print_vote_reminder.sh
@@ -6,11 +6,11 @@ source ${SCRIPT_DIR}/lib/discord_utils.sh
 print_vote_reminder()
 {
   tellraw_in_minecraft "" ""
-  tellraw_in_minecraft "Don't forget to upvote the server on these server lists!" "light_purple" ""
+  tellraw_in_minecraft "Do you like our server? Vote for us on these server lists!" "light_purple" ""
   tellraw_in_minecraft "https://www.minecartrapidtransit.net/server-voting-lists" "green" "" "https://www.minecartrapidtransit.net/server-voting-lists"
   tellraw_in_minecraft "" ""
 
-  send_message_to_discord ":ballot_box: **Don't forget to upvote the server on these server lists!** :ballot_box:" "Vote Reminder"
+  send_message_to_discord ":ballot_box: **Do you like our server? Vote for us on these server lists!** :ballot_box:" "Vote Reminder"
   send_message_to_discord ":ballot_box: **https://www.minecartrapidtransit.net/server-voting-lists** :ballot_box:" "Vote Reminder"
 }
 
